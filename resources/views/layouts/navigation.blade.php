5ebe05f67586836b81842c76e28fb461330dedf9
topstar116 <makhealth923@gmail.com>
2023-02-27 14:26:45 +0900

    up

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -18,9 +18,12 @@
                     </x-nav-link>
                 </div>
 
+                @if(Auth::user()->role != 'admin')
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex items-center">
 
+                
                     <x-dropdown align="right" width="48">
+                        
                         <x-slot name="trigger">
                             <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
                                 <div>受検依頼一覧</div>
@@ -80,7 +83,7 @@
 
                 </div>
 
-                @if(Auth::user()->role == 'admin')
+                @else
                 <!-- <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex items-center">
 
                     <x-dropdown align="right" width="48">
