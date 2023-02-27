11658926fa7d804977eb3a69151474f3eb3f1f08
topstar116 <makhealth923@gmail.com>
2023-02-27 14:30:32 +0900

    uup

--- resources/views/quiz/admin/sales_result.blade.php
+++ resources/views/quiz/admin/sales_result.blade.php
@@ -64,7 +64,7 @@
                                                         <p class="text-gray-600 whitespace-no-wrap">{{ $result->initName_f }}</p>
                                                     </td>
                                                     <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                                        <p class="text-gray-600 whitespace-no-wrap">採用</p>
+                                                        <p class="text-gray-600 whitespace-no-wrap">営業</p>
                                                     </td>
                                                     <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                                                         <p class="text-gray-900 whitespace-no-wrap">{{ $result->created_at }}</p>
