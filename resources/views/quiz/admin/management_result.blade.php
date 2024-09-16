e407b9d0985700c3b65a536c0d9952df29be3ea2
topshinjiron <topshinjiron@gmail.com>
2024-09-16 16:10:13 +0900

    modify

--- resources/views/quiz/admin/management_result.blade.php
+++ resources/views/quiz/admin/management_result.blade.php
@@ -1,7 +1,4 @@
 <x-app-layout>
-
-
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -78,7 +75,12 @@
                                                         </td>
                                                         <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                                                             <p class="text-gray-900 whitespace-nowrap">
-                                                                {{ $result->created_at }}</p>
+                                                                @if ($result->updated_at)
+                                                                    {{ $result->updated_at }}
+                                                                @else
+                                                                    {{ $result->created_at }}
+                                                                @endif
+                                                            </p>
                                                         </td>
                                                         <td
                                                             class="px-5 py-5 flex flex-col sm:flex-row gap-2 border-b border-gray-200 bg-white text-sm">
